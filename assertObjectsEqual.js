17431621d8ec764354c93103b0445a1e6b9c5da8
masinette <samuelstoniann@gmail.com>
2021-02-08 18:11:18 -0500

    added import lines

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,42 +1,5 @@
-const eqArrays = function(arrayOne, arrayTwo){
-  //check arrays are not the same length
-  if (arrayOne.length !== arrayTwo.length){
-    return false;
-  }
-  //loop through array and check if items at each index match
-  for (let i = 0; i < arrayOne.length; i++){
-    if (arrayOne[i] !== arrayTwo[i]){
-      return false;
-    }
-  }
-  return true;
-}
-const eqObjects = function(object1, object2) {
-  
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  }
-
-  //use FOR OF, because the loop is for an array
-  for (let item of Object.keys(object1)) {
-// console.log("object1", object1[item])
-    
-    if (Array.isArray(object1[item]) && Array.isArray(object2[item])) {
-      if ((eqArrays(object1[item], object2[item])) === false) {
-        return false;
-        } else{
-        return true;
-        }
-      }
-
-    if(object1[item] !== object2[item]) {
-      return false;
-    }
-
-
-  }
-  return true;
-}
+const eqArrays = require("./eqArrays.js")
+const eqObjects = require("./eqObjects.js")
 
 
 /* 
