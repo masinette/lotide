160870add93ed8bfab78587d176e2dfadbf0f900
masinette <samuelstoniann@gmail.com>
2021-02-08 15:35:45 -0500

    refactored with Mocha framework, Chai library

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,18 @@
+const { assert } = require("chai");
 const middle = require("../middle.js");
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  }),
+  it("returns the middle number for array with odd length, [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  }),
+  it("returns the two middle numbers for array with even length [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+}); 
+
 
 //TEST CODE
 middle([1]) // => []
