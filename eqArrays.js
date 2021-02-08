e90b148318c341a98b1c49b577b37f3d902aedf2
masinette <samuelstoniann@gmail.com>
2021-02-08 13:30:30 -0500

    refactored with modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,5 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = require("./assertEqual.js");
 
-  if (actual === expected){
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  else{
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 // implement a function eqArrays which takes in two arrays 
 // and returns true or false, based on a perfect match.
 const eqArrays = function(arrayOne, arrayTwo){
@@ -23,52 +16,5 @@ const eqArrays = function(arrayOne, arrayTwo){
   return true;
 }
 
+module. exports = eqArrays;
 
-
-
-
-
-
-
-
-
-
-
-
-// const eqArrays = function(arrayOne, arrayTwo){
- 
-//   let answerArray = [];
-
-//   //loop through arrayOne and arrayTwo and push boolean answer to new array
-//   for ( let i = 0; i < arrayOne.length; i++){
-//     if ((arrayOne[i]) === (arrayTwo[i])){
-//       answerArray.push(true);
-//     } else{
-//       answerArray.push(false);
-//     }
-//   }
-
-//   //loop through answerArray and return true or false
-//   for ( let i = 0; i < arrayOne.length; i++){
-//     if (answerArray[i] === false){
-//       console.log("false")
-//       return false;
-//     } 
-//   }
-//   // console.log("true");
-//   return true;
-// }
-// "remember to put return 
-//outside of loop when testing 
-//a whole array, so it runs 
-//through the entire loop first"
-
-// test cases
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
